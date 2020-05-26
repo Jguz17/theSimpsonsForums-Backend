e7ef1a7fe1dd7e257c76eb07d0222e46aad2a878
jguz17 <johnathanguzman17@gmail.com>
2020-05-26 05:54:16 -0500

    added 'create' feature to backend

--- app/controllers/simpsons_controller.rb
+++ app/controllers/simpsons_controller.rb
@@ -8,4 +8,15 @@ class SimpsonsController < ApplicationController
         simpson = Simpson.find(params[:id])
         render json: SimpsonSerializer.new(simpson)
     end
+
+    def create 
+        name = params[:name]
+        quote = params[:quote]
+        image = params[:image]
+
+        simpson = Simpson.create(name: name, quote: quote, image: image)
+        puts simpson
+
+        render json: simpson
+    end
 end
